9a1b94a105fea3f390fb58179e5ca55240c3c8c1
idith almog <almog.idith@gmail.com>
2020-12-15 19:20:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,22 +12,35 @@ class Student
     @linkedin=student_hash[:linkedin]
     @github=student_hash[:github]
     @blog=student_hash[:blog]
+<<<<<<< HEAD
     @profile_quote=student_hash[:profile_quote]
+=======
+    @profime_quote=student_hash[:profile_quote]
+>>>>>>> 0d7d21184831e9a3a694f83c68bb7d2e0ab5ffc2
     @bio=student_hash[:bio]
     @profile_url=student_hash[:profile_url]
     @@all<<self
   end
 
   def self.create_from_collection(students_array)
+<<<<<<< HEAD
 
     students_array.each do |student|
     Student.new(student)
+=======
+    students_array.each do |student|
+      firstlast=student[:name].split
+       binding.pry
+      student[:profile_url]="https://learn-co-curriculum.github.io/student-scraper-test-page/students/"+"#{firstlast[0]}-#{firstlast[1]}"+".html"
+      Scraper.scrape_profile_page(student[:profile_url])
+>>>>>>> 0d7d21184831e9a3a694f83c68bb7d2e0ab5ffc2
     end
 
   end
 
   def add_student_attributes(attributes_hash)
 
+<<<<<<< HEAD
     #firstlast=self.name.split
     #if self.profile_url==nil
         #self.profile_url="students/"+"#{firstlast[0]}-#{firstlast[1]}"+".html"
@@ -66,6 +79,12 @@ class Student
 
   def self.all
     @@all
+=======
+  end
+
+  def self.all
+
+>>>>>>> 0d7d21184831e9a3a694f83c68bb7d2e0ab5ffc2
   end
 
 end
